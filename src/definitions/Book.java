892e55fc7c14f4960e59f9db63513b4b04682f3f
Harsh Gupta <harsh.gupta_cs19@gla.ac.in>
2020-08-30 07:08:29 -0400

    create getters and setters for all fields.

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -24,6 +24,31 @@ public class Book {
         this.bookName = bookName;
     }
 
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getISBNNumber() {
+        return ISBNNumber;
+    }
+
+    public void setISBNNumber(String ISBNNumber) {
+        this.ISBNNumber = ISBNNumber;
+    }
+
+
 
 
 }
