74c02215ff7920159b0933627d1183522ee53966
Harsh Gupta <harsh.gupta_cs19@gla.ac.in>
2020-08-30 08:13:02 -0400

    import Scanner class in FrontDesk.java class.

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -10,6 +10,9 @@ import definitions.Book;
 import definitions.Library;
 import definitions.Student;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class FrontDesk {
     private static final int ISSUE_NEW_BOOK = 1;
     private static final int RETURN_BOOK = 2;
@@ -17,6 +20,29 @@ public class FrontDesk {
     private static final int EXIT = 4;
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int customerInput;
+        String studentName;
+        long universityRollNumber;
+        String nameOfBook;
+        String authorName;
+        String ISBNNumber;
+        System.out.println("Enter your Name\r");
+        studentName = scanner.nextLine();
+        System.out.println("Enter your university roll number\r");
+        universityRollNumber = scanner.nextLong();
+        scanner.nextLine();
+        System.out.println("Enter book name you have assigned earlier\r");
+        nameOfBook = scanner.nextLine();
+        System.out.println("Enter book's author Name");
+        authorName = scanner.nextLine();
+        System.out.println("Enter 13 digit ISBN number");
+        ISBNNumber = scanner.nextLine();
+        Book book = new Book(nameOfBook, authorName, ISBNNumber);
+        Book[] issuedBooks = {book};
+        Student student = new Student(studentName, universityRollNumber, issuedBooks.length, issuedBooks);
+        Library library = new Library();
+        System.out.println("******************************************************************************");
 
     }
 
