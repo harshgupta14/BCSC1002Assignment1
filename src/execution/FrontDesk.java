df39d0c54ea14ac47cd9f86ea0baa8fc8536c3e5
Harsh Gupta <harsh.gupta_cs19@gla.ac.in>
2020-08-30 08:04:13 -0400

    create private field ISSUE_NEW_BOOK,RETURN_BOOK,SHOW_ALL_ISSUED_BOOKS,EXIT in FrontDesk.java

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -11,6 +11,9 @@ import definitions.Library;
 import definitions.Student;
 
 public class FrontDesk {
-
+    private static final int ISSUE_NEW_BOOK = 1;
+    private static final int RETURN_BOOK = 2;
+    private static final int SHOW_ALL_ISSUED_BOOKS = 3;
+    private static final int EXIT = 4;
 
 }
