f2a1c72636c4341367fb1929a5fdf97583833d04
Harsh Gupta <harsh.gupta_cs19@gla.ac.in>
2020-08-30 07:32:24 -0400

    create toString(),equals()and hashCode() methods.

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -33,6 +33,26 @@ public class Library {
         this.availableBooks = availableBooks;
     }
 
+    @Override
+    public String toString() {
+        return "Library{" +
+                "availableBooks=" + Arrays.toString(availableBooks) +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Library)) return false;
+        Library library = (Library) o;
+        return Arrays.equals(getAvailableBooks(), library.getAvailableBooks());
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(getAvailableBooks());
+    }
+
 
 
     }
