630ba9d5b2dcc4031ac97c5ecf8ecd25782818f4
Harsh Gupta <harsh.gupta_cs19@gla.ac.in>
2020-08-30 07:37:58 -0400

    create returnBook()method.

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -6,5 +6,126 @@
  * */
 package definitions;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Student {
+    private String studentFullName;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private long universityRollNumber;
+    private int numberOfBooksIssuedToStudent;
+    private Book[] nameOfBooksIssuedToStudent;
+
+    public Student() {
+    }
+
+    public Student(String studentFullName, long universityRollNumber, int numberOfBooksIssuedToStudent, Book[] nameOfBooksIssuedToStudent) {
+        this.studentFullName=studentFullName;
+        this.universityRollNumber = universityRollNumber;
+        this.numberOfBooksIssuedToStudent = numberOfBooksIssuedToStudent;
+        this.nameOfBooksIssuedToStudent = nameOfBooksIssuedToStudent;
+    }
+
+    public Student(String firstName, String middleName, String lastName, long universityRollNumber, int numberOfBooksIssuedToStudent, Book[] nameOfBooksIssuedToStudent) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.universityRollNumber = universityRollNumber;
+        this.numberOfBooksIssuedToStudent = numberOfBooksIssuedToStudent;
+        this.nameOfBooksIssuedToStudent = nameOfBooksIssuedToStudent;
+        this.studentFullName = firstName + middleName + lastName;
+
+    }
+
+    public String getStudentFullName() {
+        return studentFullName;
+    }
+
+    public void setStudentFullName(String studentFullName) {
+        this.studentFullName = studentFullName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public long getUniversityRollNumber() {
+        return universityRollNumber;
+    }
+
+    public void setUniversityRollNumber(long universityRollNumber) {
+        this.universityRollNumber = universityRollNumber;
+    }
+
+    public int getNumberOfBooksIssuedToStudent() {
+        return numberOfBooksIssuedToStudent;
+    }
+
+    public void setNumberOfBooksIssuedToStudent(int numberOfBooksIssuedToStudent) {
+        this.numberOfBooksIssuedToStudent = numberOfBooksIssuedToStudent;
+    }
+
+    public Book[] getNameOfBooksIssuedToStudent() {
+        return nameOfBooksIssuedToStudent;
+    }
+
+    public void setNameOfBooksIssuedToStudent(Book[] nameOfBooksIssuedToStudent) {
+        this.nameOfBooksIssuedToStudent = nameOfBooksIssuedToStudent;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "studentFullName='" + studentFullName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", universityRollNumber=" + universityRollNumber +
+                ", numberOfBooksIssuedToStudent=" + numberOfBooksIssuedToStudent +
+                ", nameOfBooksIssuedToStudent=" + Arrays.toString(nameOfBooksIssuedToStudent) +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Student)) return false;
+        Student student = (Student) o;
+        return getUniversityRollNumber() == student.getUniversityRollNumber() &&
+                getNumberOfBooksIssuedToStudent() == student.getNumberOfBooksIssuedToStudent() &&
+                Objects.equals(getStudentFullName(), student.getStudentFullName()) &&
+                Objects.equals(getFirstName(), student.getFirstName()) &&
+                Objects.equals(getMiddleName(), student.getMiddleName()) &&
+                Objects.equals(getLastName(), student.getLastName()) &&
+                Arrays.equals(getNameOfBooksIssuedToStudent(), student.getNameOfBooksIssuedToStudent());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(getStudentFullName(), getFirstName(), getMiddleName(), getLastName(), getUniversityRollNumber(), getNumberOfBooksIssuedToStudent());
+        result = 31 * result + Arrays.hashCode(getNameOfBooksIssuedToStudent());
+        return result;
+    }
 }
--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -6,8 +6,70 @@
  * */
 package execution;
 
-public class FrontDesk {
-    public static void main(String[] args) {
+import definitions.Book;
+import definitions.Library;
+import definitions.Student;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class FrontDesk {
+    private static final int ISSUE_NEW_BOOK = 1;
+    private static final int RETURN_BOOK = 2;
+    private static final int SHOW_ALL_ISSUED_BOOKS = 3;
+    private static final int EXIT = 4;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int userInput;
+        String studentName;
+        long universityRollNumber;
+        String nameOfBook;
+        String authorName;
+        String ISBNNumber;
+        System.out.println("Enter your Name\r");
+        studentName = scanner.nextLine();
+        System.out.println("Enter your university roll number\r");
+        universityRollNumber = scanner.nextLong();
+        scanner.nextLine();
+        System.out.println("Enter book name you have assigned earlier\r");
+        nameOfBook = scanner.nextLine();
+        System.out.println("Enter book author Name");
+        authorName = scanner.nextLine();
+        System.out.println("Enter 13 digit ISBN number");
+        ISBNNumber = scanner.nextLine();
+        Book book = new Book(nameOfBook, authorName, ISBNNumber);
+        Book[] issuedBooks = {book};
+        Student student = new Student(studentName, universityRollNumber, issuedBooks.length, issuedBooks);
+        Library library = new Library();
+        System.out.println("********************************************************************************");
+        do {
+            System.out.println("-=-=--=-=-\"Welcome To The Front Desk\"-=-=--=-=-\n");
+            System.out.println("How may I help you today?\n");
+            System.out.println("1. Issue a new book for me.\n");
+            System.out.println("2. Return a previously issues book for me.\n");
+            System.out.println("3. Show me all my issues books.\n");
+            System.out.println("4. Exit.\n");
+            userInput = scanner.nextInt();
+            switch (userInput) {
+                case ISSUE_NEW_BOOK:
+                    Scanner scanner1=new Scanner(System.in);
+                    String bookName;
+                    System.out.println("Enter the name of book you want");
+                    bookName = scanner1.nextLine();
+                    library.issueBook(bookName);
+                    break;
+                case RETURN_BOOK:
+                    library.returnBook();
+                    break;
+                case SHOW_ALL_ISSUED_BOOKS:
+                    library.issuedBooksToStudent(studentName, universityRollNumber, issuedBooks);
+                    break;
+                default:
+                    break;
+            }
+        }
+        while (userInput != EXIT);
+        scanner.close();
     }
 }
